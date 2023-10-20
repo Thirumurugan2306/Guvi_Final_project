a178ef42403a12ffaf9eb750443ce5874eb5e940
THIRUMURUGAN T <thirum667@gmail.com>
2023-10-20 11:19:26 +0530

    Update app.py

--- instagram_influencer/app.py
+++ instagram_influencer/app.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import re
 
 # Load the dataset (assuming it's in a CSV file named 'Influencer.csv')
-df = pd.read_csv('instagram_influencer/Influencer.csv')
+df = pd.read_csv('Influencer.csv')
 
 # Rename columns and clean data
 column_mapping = {col: col.replace(' ', '_') for col in df.columns}
@@ -43,49 +43,60 @@ for column in columns_to_convert:
 # Streamlit app
 st.title('Instagram Influencer Analysis')
 
-# Correlation Heatmap
-st.subheader('Correlation Heatmap')
-numeric_columns = df.select_dtypes(include=['int64', 'float64'])
-correlation_matrix = numeric_columns.corr()
-st.plotly_chart(px.imshow(correlation_matrix, color_continuous_scale='blues'))
 
-# Frequency Distribution of Influence Score
-st.subheader('Frequency Distribution of Influence Score')
-fig = px.histogram(df, x='influence_score', nbins=20, title='Frequency Distribution of Influence Score')
-st.plotly_chart(fig)
+# Sidebar for selecting visuals
+selected_visual = st.sidebar.selectbox("Select Visual", ["Frequency Distribution - Influence Score",
+                                                        "Frequency Distribution - Followers",
+                                                        "Frequency Distribution - Posts",
+                                                        "Number of Influencers by Country",
+                                                        "Top 10 Influencers by Followers",
+                                                        "Correlation Heatmap",
+                                                        "Scatter Plots"])
 
-# Frequency Distribution of Followers
-st.subheader('Frequency Distribution of Followers')
-fig = px.histogram(df, x='followers', nbins=20, title='Frequency Distribution of Followers')
-st.plotly_chart(fig)
+# Display selected visuals based on the selection
+if selected_visual == "Frequency Distribution - Influence Score":
+    st.subheader('Frequency Distribution of Influence Score')
+    fig = px.histogram(df, x='influence_score', nbins=20, title='Frequency Distribution of Influence Score')
+    st.plotly_chart(fig)
 
-# Frequency Distribution of Posts
-st.subheader('Frequency Distribution of Posts')
-fig = px.histogram(df, x='posts', nbins=20, title='Frequency Distribution of Posts')
-st.plotly_chart(fig)
+elif selected_visual == "Frequency Distribution - Followers":
+    st.subheader('Frequency Distribution of Followers')
+    fig = px.histogram(df, x='followers', nbins=20, title='Frequency Distribution of Followers')
+    st.plotly_chart(fig)
 
-# Number of Instagram Influencers by Country
-st.subheader('Number of Instagram Influencers by Country')
-country_counts = df['country_or_region'].value_counts()
-fig = px.bar(country_counts, x=country_counts.index, y=country_counts.values, orientation='h',
-             title='Number of Instagram Influencers by Country')
-st.plotly_chart(fig)
+elif selected_visual == "Frequency Distribution - Posts":
+    st.subheader('Frequency Distribution of Posts')
+    fig = px.histogram(df, x='posts', nbins=20, title='Frequency Distribution of Posts')
+    st.plotly_chart(fig)
 
-# Top 10 Influencers based on Followers
-st.subheader('Top 10 Influencers based on Followers')
-top_followers = df.sort_values(by='followers', ascending=False).head(10)
-fig = px.bar(top_followers, x='followers', y='channel_info', orientation='h',
-             title='Top 10 Influencers based on Followers')
-st.plotly_chart(fig)
+elif selected_visual == "Number of Influencers by Country":
+    st.subheader('Number of Instagram Influencers by Country')
+    country_counts = df['country_or_region'].value_counts()
+    fig = px.bar(country_counts, y=country_counts.index, x=country_counts.values, orientation='h',
+                 title='Number of Instagram Influencers by Country')
+    st.plotly_chart(fig)
 
-# Scatter plots
-st.subheader('Scatter Plots')
-scatter_fig1 = px.scatter(df, x='followers', y='total_likes', title='Relationship between Followers and Total Likes')
-scatter_fig2 = px.scatter(df, x='followers', y='influence_score', title='Relationship between Followers and Influence Score')
-scatter_fig3 = px.scatter(df, x='posts', y='avg_likes', title='Relationship between Posts and Average Likes')
-scatter_fig4 = px.scatter(df, x='posts', y='influence_score', title='Relationship between Posts and Influence Score')
+elif selected_visual == "Top 10 Influencers by Followers":
+    st.subheader('Top 10 Influencers based on Followers')
+    top_followers = df.sort_values(by='followers', ascending=False).head(10)
+    fig = px.bar(top_followers, x='followers', y='channel_info', orientation='h',
+                 title='Top 10 Influencers based on Followers')
+    st.plotly_chart(fig)
 
-st.plotly_chart(scatter_fig1)
-st.plotly_chart(scatter_fig2)
-st.plotly_chart(scatter_fig3)
-st.plotly_chart(scatter_fig4)
+elif selected_visual == "Correlation Heatmap":
+    st.subheader('Correlation - Heatmap')
+    numeric_columns = df.select_dtypes(include=['int64', 'float64'])
+    correlation_matrix = numeric_columns.corr()
+    st.plotly_chart(px.imshow(correlation_matrix, color_continuous_scale='blues'))
+
+elif selected_visual == "Scatter Plots":
+    st.subheader('Scatter Plots')
+    scatter_fig1 = px.scatter(df, x='followers', y='total_likes', title='Relationship between Followers and Total Likes')
+    scatter_fig2 = px.scatter(df, x='followers', y='influence_score', title='Relationship between Followers and Influence Score')
+    scatter_fig3 = px.scatter(df, x='posts', y='avg_likes', title='Relationship between Posts and Average Likes')
+    scatter_fig4 = px.scatter(df, x='posts', y='influence_score', title='Relationship between Posts and Influence Score')
+
+    st.plotly_chart(scatter_fig1)
+    st.plotly_chart(scatter_fig2)
+    st.plotly_chart(scatter_fig3)
+    st.plotly_chart(scatter_fig4)
